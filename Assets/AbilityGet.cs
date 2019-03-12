ff9cd700179defa43574ef5669a1fbf428b0dde7
maxFischer42 <42ducksable1@gmail.com>
2019-03-12 09:40:26 -0700

    World 2 Hub and Transition

---
+++ Assets/AbilityGet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AbilityGet : MonoBehaviour {
+
+    public Image fade;
+    public bool fading;
+    public string SceneToLoad;
+    public GameObject particles;
+    public SpriteRenderer spriteRender;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!fading)
+            return;
+            Color newColor = fade.color;
+            newColor.a += 0.01f;
+            fade.color = newColor;
+        if(newColor.a >= 1)
+        {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+        
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            spriteRender.enabled = false;
+            particles.SetActive(false);
+            fading = true;
+        }
+    }
+}
